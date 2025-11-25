4ec8909d5b02df01dc0d5b1f025ac1818aa1c6ae
DVonDOOM <dcooke@highpoint.edu>
2025-11-25 00:30:57 -0500

    Mostly completed (just missing few sounds) and added "Replay Intro" button to main menu scene

---
+++ Intro/intro.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+
+func go_to_menu():
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
--- mainmenu.gd
+++ mainmenu.gd
@@ -21,3 +21,7 @@ func _on_cat_button_pressed() -> void:
 
 func _on_dog_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://dogtest.tscn")
+
+
+func _on_replay_intro_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Intro/Intro.tscn")
