3eeefa92810aaeda829992a5d396da73d8def56c
DVonDOOM <dcooke@highpoint.edu>
2025-12-03 09:34:18 -0500

    skip intro

--- Intro/intro.gd
+++ Intro/intro.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func go_to_menu():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_skip_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
